43f4e7d9e8cd58819249891bc0544a3cb64972a8
Gonzalo Ruiz <63617887+Gonzalo261@users.noreply.github.com>
2022-05-12 09:32:57 -0300

    portal_cristal_initial

---
+++ portal_cristal/portal_cristal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace portal_cristal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
